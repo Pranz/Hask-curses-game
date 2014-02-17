da3ce34ab58f7ff8564c67254573a5f1dc88c9d6
Jesper Fridefors <jesper.fridefors@gmail.com>
2014-02-17 19:06:36 +0100

    'Is' Can now be indexed by multiple constraints, Is' and Is'' take two respective 3 constraints

--- Classes.hs
+++ Classes.hs
@@ -17,17 +17,18 @@ import Data.Function (on)
 
 import Util
 
-data Is  c     = forall a. (c a)        => Is  a
-data Is' c1 c2 = forall a. (c1 a, c2 a) => Is' a
+data Is  c        = forall a. (c a)               => Is   a
+data Is' c1 c2    = forall a. (c1 a, c2 a)        => Is'  a
+data Is'' c1 c2 c3 = forall a. (c1 a, c2 a, c3 a) => Is'' a
 
-conmap :: forall b c.          (forall a.  c a               => a -> b) -> Is c -> b
+conmap :: forall b c.          (forall a.  c a               => a -> b) -> Is   c -> b
 conmap f (Is x) = f x
 
-conmap' :: forall b c1 c2.     (forall a. (c1 a, c2 a)       => a -> b) -> Is' c1 c2 -> b
+conmap' :: forall b c1 c2.     (forall a. (c1 a, c2 a)       => a -> b) -> Is'  c1 c2 -> b
 conmap' f (Is' x) = f x
 
 conmap'' :: forall b c1 c2 c3. (forall a. (c1 a, c2 a, c3 a) => a -> b) -> Is'' c1 c2 c3 -> b
-conmap'' f (Is' x) = f x
+conmap'' f (Is'' x) = f x
 
 class Representable e where
     char  :: Getter e Char
